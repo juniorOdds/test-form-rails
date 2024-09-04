ef0d39c8c0cb59826f5be2a17e9736c15b798b9f
juniorOdds <junior@odds.team>
2024-09-04 21:07:51 +0700

    rename routes {register => dashboard}, {register/:id => dashboard/:id}, {register/new => registerform}

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
-  get "register", to: "user#index"
-  get "register/:id", to: "user#list", as: :user_list
-  get "register/new", to: "user#new", as: :register_user
+  get "dashboard", to: "user#index"
+  get "dashboard/:id", to: "user#list", as: :user_list
+  get "registerform", to: "user#new", as: :register_user
   # Defines the root path route ("/")
   # root "posts#index"
 end
