cb1621c3221b599ddfc3e147a8a6f77a0214937d
juniorOdds <junior@odds.team>
2024-09-05 09:18:52 +0700

    add validate to registerform if fields are blank will show error

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,7 +13,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_list_path(@user)
+      redirect_to '/dashboard'
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   validates :first_name, presence: { message: "can't be blank" }
+  validates :last_name, presence: { message: "can't be blank" }
+  validates :gender, presence: { message: "can't be blank" }
+  validates :birth_day, presence: { message: "can't be blank" }
+  validates :email, presence: { message: "can't be blank" }
+  validates :phone_number, presence: { message: "can't be blank" }
+  validates :subject, presence: { message: "can't be blank" }
   
 end
