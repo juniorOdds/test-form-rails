63751ac0ca31a4edfa7c3f0ebfadba7ce27a172f
juniorOdds <junior@odds.team>
2024-09-16 22:46:38 +0700

    fix bux register form

---
+++ app/javascript/controllers/register_modal_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="register-modal"
+export default class extends Controller {
+  connect() {
+
+  }
+
+  hide_modal(e) {
+    const registerModal = document.getElementById("modalRegister")
+    if (registerModal) {
+      registerModal.style.display = "none"
+    }
+  }
+}
